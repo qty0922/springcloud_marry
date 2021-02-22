fbec09833e0ddf5ec609515cf2fa53438d5214a5
qty0922 <1506585406@qq.com>
2021-02-22 09:38:17 +0800

    Eureka开发时快速移除失效服务

--- springcloud-eureka-server/src/main/java/com/ytc/SpringcloudEurekaServerApplication.java
+++ springcloud-eureka-server/src/main/java/com/ytc/SpringcloudEurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer //客户端
+@EnableEurekaServer //服务端
 public class SpringcloudEurekaServerApplication {
 
     public static void main(String[] args) {
