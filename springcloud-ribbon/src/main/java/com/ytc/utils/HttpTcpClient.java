a5f49754dda63d55ccc328906453642ce6122200
qty0922 <1506585406@qq.com>
2021-02-22 14:49:48 +0800

    添加负载均衡——ribbon实例 + 测试实例

---
+++ springcloud-ribbon/src/main/java/com/ytc/utils/HttpTcpClient.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: HttpTcpClient
+ * Author:   15065
+ * Date:     2021/2/22 14:25
+ * Description:
+ * History:
+ * qty               <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.ytc.utils;
+
+import com.google.common.collect.Lists;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.LoadBalancerBuilder;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.reactive.LoadBalancerCommand;
+import com.netflix.loadbalancer.reactive.ServerOperation;
+import rx.Observable;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * 〈实例：演示了 Ribbon 如何去做负载操作〉<br>
+ * 〈调用接口用的最底层的 HttpURLConnection〉
+ *
+ * @author 15065
+ * @create 2021/2/22
+ * @since 1.0.0
+ */
+public class HttpTcpClient {
+    public static void main(String[] args) {
+        // 服务列表
+        List<Server> serverList = Lists.newArrayList(new Server("localhost", 8081), new Server("localhost", 8082));
+        // 构建负载实例
+        ILoadBalancer loadBalancer = LoadBalancerBuilder.newBuilder().buildFixedServerListLoadBalancer(serverList);
+
+        // 调用 5 次来测试效果
+        for(int i = 0;i<5;i++) {
+            String result = LoadBalancerCommand.<String>builder().withLoadBalancer(loadBalancer).build()
+                    .submit(new ServerOperation<String>() {
+                        public Observable<String> call(Server server) {
+                            try {
+                                String addr = "http://" + server.getHost() + ":" + server.getPort() + "/hello";
+                                System.out.println(" 调用地址：" + addr);
+                                URL url = new URL(addr);
+                                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                                conn.setRequestMethod("GET");
+                                conn.connect();
+                                InputStream in = conn.getInputStream();
+                                byte[] data = new byte[in.available()];
+                                in.read(data);
+                                return Observable.just(new String(data));
+                            } catch (Exception e) {
+                                return Observable.error(e);
+                            }
+                        }
+                    }).toBlocking().first();
+            System.out.println(" 调用结果：" + result);
+        }
+    }
+}
---
+++ springcloud-ribbon/src/test/java/com/ytc/SpringcloudRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytc;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
