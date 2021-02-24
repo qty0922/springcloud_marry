99568297c8e2109638e026dd439117b3a1dd29b6
qty0922 <1506585406@qq.com>
2021-02-24 10:11:47 +0800

    Hystrix —— 容错处理

--- springcloud-hystrix/src/main/java/com/ytc/SpringcloudHystrixApplication.java
+++ springcloud-hystrix/src/main/java/com/ytc/SpringcloudHystrixApplication.java
@@ -2,12 +2,22 @@ package com.ytc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+//注意：@EnableHystrix 中包含了 @EnableCircuitBreaker。
+@EnableHystrix
+//@EnableCircuitBreaker
 public class SpringcloudHystrixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudHystrixApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
---
+++ springcloud-hystrix/src/main/java/com/ytc/controller/DemoController.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: DemoController
+ * Author:   15065
+ * Date:     2021/2/24 9:40
+ * Description:
+ * History:
+ * qty               <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.ytc.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 15065
+ * @create 2021/2/24
+ * @since 1.0.0
+ */
+@RestController
+public class DemoController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/callHello")
+    @HystrixCommand(fallbackMethod = "defaultCallHello")
+    public String callHello(){
+        String result = restTemplate.getForObject("http://springcloud-provider/hello", String.class);
+        return result;
+    }
+
+    public String defaultCallHello() {
+        return "不好意思，报错了！";
+    }
+}
