726931f5571ec2c8289c68f661c89e03c2bddb5b
qty0922 <1506585406@qq.com>
2021-02-24 14:10:05 +0800

    feign + Hystrix —— 实时监控（断路监控仪表盘）

--- springcloud-feign/src/main/java/com/ytc/SpringcloudFeignApplication.java
+++ springcloud-feign/src/main/java/com/ytc/SpringcloudFeignApplication.java
@@ -1,17 +1,34 @@
 package com.ytc;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients //开启feign功能
+@EnableHystrix //开启 Hystrix功能
+@EnableCircuitBreaker   //开启断路器功能
+@EnableHystrixDashboard //开启断路监控仪表盘
 public class SpringcloudFeignApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudFeignApplication.class, args);
+    }
+
+    //断路器：需加入该关键代码
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
     }
 
 }
