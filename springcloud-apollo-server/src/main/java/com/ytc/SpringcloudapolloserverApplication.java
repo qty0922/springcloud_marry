4bd553f373b39e78e5489afc558a0905e51644fe
qty0922 <1506585406@qq.com>
2021-03-02 16:38:53 +0800

    Apollo —— 服务端设计原理(实例：模拟配置更新)

--- springcloud-apollo-server/src/main/java/com/ytc/SpringcloudapolloserverApplication.java
+++ springcloud-apollo-server/src/main/java/com/ytc/SpringcloudapolloserverApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudapolloclientApplication {
+public class SpringcloudapolloserverApplication {
 
     public static void main(String[] args) {
         // 指定环境(仅供开发演示用, 不能用于生产环境))
         System.setProperty("env", "DEV");
-        SpringApplication.run(SpringcloudapolloclientApplication.class, args);
+        SpringApplication.run(SpringcloudapolloserverApplication.class, args);
     }
 
 }
